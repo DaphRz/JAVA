f6c631cf81a9168b6c27ad77e9ea20e1e48122aa
DaphRz <159149963+DaphRz@users.noreply.github.com>
2025-02-26 10:48:46 -0300

    ok

--- Conta/src/main/java/com/mycompany/conta/ClasseConta.java
+++ Conta/src/main/java/com/mycompany/conta/ClasseConta.java
@@ -11,16 +11,44 @@ package com.mycompany.conta;
 public class ClasseConta {
     int numero;
     String nome;
-    double limite;
+    double limite1, limite2;
     double saldo;
     
     double deposito;
         
     /*public*/ void sacar(double v){  // v = valor
-        saldo = saldo - v;
+        saldo = saldo - v; // saldo -= v
     }
     
     void depositar(double v){
-        saldo = saldo + v;
+        saldo = saldo + v; // saldo += v
+    }
+    
+    void verificacaoSaque (double saque) {
+        if (saque >= 0 && ((numero == 1 && saque <= limite1) || (numero == 2 && saque <= limite2))) {
+            sacar(saque);
+        }
+        else if (saque > limite1 && numero == 1) {
+            System.out.println("\nError! Seu Limite de Saque é de R$800,00.");
+            System.exit(0);
+        }
+        else if (saque > limite2 && numero == 2) {
+            System.out.println("\nError! Seu Limite de Saque é de R$10,00.");
+            System.exit(0);
+        }
+        else {
+            System.out.println("\nError. Digite um Valor Válido!");
+            System.exit(0);
+        }
+    }
+    
+    void verificacaoDeposito () {
+        if (deposito >= 0) {
+            depositar(deposito);
+        }
+        else {
+            System.out.println("\nError. Digite um Valor Válido!");
+            System.exit(0);
+        }
     }
 }
--- Conta/src/main/java/com/mycompany/conta/Conta.java
+++ Conta/src/main/java/com/mycompany/conta/Conta.java
@@ -18,13 +18,18 @@ public class Conta {
         conta = new ClasseConta();
         
         Scanner scan = new Scanner(System.in);
+        
+        // posso declarar todos os objetos aqui: nome, numero, ...
+        
+        conta.limite1 = 800;
+        conta.limite2 = 10;
     
         System.out.println("\n\tMenu da Conta\n");
         
         System.out.print("Digite seu Nome: ");
         conta.nome = scan.nextLine();
         
-        if(conta.nome.equals("Daphne") || conta.nome.equals("daphne")){
+        if(conta.nome.equalsIgnoreCase("Daphne")){
         }
         else {
             System.out.println("\nError. Digite um Nome Válido!");
@@ -36,30 +41,70 @@ public class Conta {
         
         try {
             conta.numero = scan.nextInt();
-            System.out.println("\n\tOK. Conta Válida");
+            
+            if (conta.numero == 1 || conta.numero == 2) {
+                System.out.println("\n\tOK. Conta Válida");
+            }
+            else {
+                System.out.println("\nError. Digite uma Conta Válida!");
+                System.exit(0);
+            }
         }
         catch (Exception e) {
-            System.out.println("Error. Digite uma Conta Válida");
+            System.out.println("\nError. Digite uma Conta Válida!");
+            System.exit(0);
         }
     
-        System.out.print("Digite o Valor do seu Saldo: ");
+        System.out.print("\nDigite o Valor do seu Saldo: ");
         conta.saldo = scan.nextDouble();
     
         System.out.print("Digite o valor do Saque: ");
         double saque = scan.nextDouble();
         
-        conta.sacar(saque);
+        conta.verificacaoSaque(saque);
+        
+                                                                                 /* if (saque >= 0 && saque <= conta.limite1 && saque <= conta.limite2) {
+                                                                                        conta.sacar(saque); }
+                                                                                    else if (saque > conta.limite1 && conta.numero == 1) {
+                                                                                        System.out.println("\nError! Seu Limite de Saque é de R$800,00.");
+                                                                                        System.exit(0); }
+                                                                                    else if (saque > conta.limite2 && conta.numero == 2) {
+                                                                                        System.out.println("\nError! Seu Limite de Saque é de R$10,00.");
+                                                                                        System.exit(0); }
+                                                                                    else {
+                                                                                        System.out.println("\nError. Digite um Valor Válido!");
+                                                                                        System.exit(0); } */
+                
+        System.out.println("\nValor da Conta Atualizado: "+ conta.saldo);
+        
+        System.out.print("\nDigite o valor do Depósito: ");
+        conta.deposito = scan.nextDouble();
+        
+                                                                                 /* if (conta.deposito >= 0) {
+                                                                                        conta.depositar(conta.deposito); }
+                                                                                    else {
+                                                                                        System.out.println("\nError. Digite um Valor Válido!");
+                                                                                        System.exit(0); } */
+        
+        conta.verificacaoDeposito();
+        
+        System.out.println("\nValor da Conta Atualizado: "+ conta.saldo);
+        
+        System.out.print("\nDigite o valor do Saque: ");
+        saque = scan.nextDouble();
+        
+        conta.verificacaoSaque(saque);
         
         System.out.println("\nValor da Conta Atualizado: "+ conta.saldo);
         
         System.out.print("\nDigite o valor do Depósito: ");
         conta.deposito = scan.nextDouble();
         
-        conta.depositar(conta.deposito);
-        
-        System.out.printf("\nValor da Conta Atualizado: "+ conta.saldo);
+        conta.verificacaoDeposito();
+
+        System.out.println("\nValor da Conta Atualizado: "+ conta.saldo);
+        System.out.println("\n\tConcluído!");
         
         scan.close();
-
     }
 }
